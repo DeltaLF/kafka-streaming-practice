d65ca762799f5868420abfaa018b4d3db7728771
Patrick <patricktsaikl@gmail.com>
2024-11-28 16:32:26 +0800

    feat/operations in kafka stream

--- greeting-streams/readme.md
+++ greeting-streams/readme.md
@@ -8,6 +8,9 @@ docker exec --interactive --tty broker kafka-topics --bootstrap-server broker:90
 # producer
 docker exec --interactive --tty broker kafka-console-producer --bootstrap-server broker:9092 --topic greetings
 
+docker exec --interactive --tty broker kafka-console-producer --bootstrap-server broker:9092 --topic greetings --property "key.separator=-" --property "parse.key=true"
+
+
 # consumer
  docker exec --interactive --tty broker kafka-console-consumer --bootstrap-server broker:9092 --topic greetings_uppercase
 ```
--- greeting-streams/src/main/java/com/learnkafkastreams/topology/GreetingsTopology.java
+++ greeting-streams/src/main/java/com/learnkafkastreams/topology/GreetingsTopology.java
@@ -7,9 +7,14 @@ import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.Printed;
 import org.apache.kafka.streams.kstream.Produced;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class GreetingsTopology {
 
     public static String GREETINGS = "greetings";
+
+    public static String GREETINGSII = "greetings2"; // for merging
 
     public static String GREETINGS_UPPERCASE = "greetings_uppercase";
 
@@ -22,12 +27,30 @@ public class GreetingsTopology {
         var greetingsStream = streamsBuilder
                 .stream(GREETINGS,
                         Consumed.with(Serdes.String(), Serdes.String()));
+        var greetingsStreamII = streamsBuilder
+                .stream(GREETINGSII,
+                        Consumed.with(Serdes.String(), Serdes.String()));
 
-        greetingsStream.print(Printed.<String, String>toSysOut().withLabel("greetingStream"));
+        var mergedStream = greetingsStream.merge(greetingsStreamII);
 
-        var modifiedStream = greetingsStream.mapValues((readOnlyKey, value) -> value.toUpperCase());
+        mergedStream.print(Printed.<String, String>toSysOut().withLabel("mergedStream"));
 
-        greetingsStream.print(Printed.<String, String>toSysOut().withLabel("modifiedStream"));
+        var modifiedStream = mergedStream
+                .filter((key, value) -> value.length() > 5)
+                .peek((key, value) -> {
+                    log.info("# peek stream key:{}, value:{}", key, value);
+                })
+                .mapValues((readOnlyKey, value) -> value.toUpperCase());
+        // .map((key, value) -> KeyValue.pair(key.toUpperCase(), value.toUpperCase()));
+        // .flatMap((key, value) -> {
+        // var splitValue = Arrays.asList(value.split(""));
+        // return splitValue
+        // .stream()
+        // .map(val -> KeyValue.pair(key, val.toUpperCase()))
+        // .collect(Collectors.toList());
+        // });
+
+        mergedStream.print(Printed.<String, String>toSysOut().withLabel("modifiedStream"));
 
         modifiedStream.to(GREETINGS_UPPERCASE, Produced.with(Serdes.String(), Serdes.String()));
 
